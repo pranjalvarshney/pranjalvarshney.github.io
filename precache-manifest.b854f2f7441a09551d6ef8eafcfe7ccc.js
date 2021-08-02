d85514989586ca354b789ab2abbf19af86672d8e
pranjalvarshney <pranjal25varshney@gmail.com>
2021-08-02 19:42:23 +0530

    Updates

---
+++ precache-manifest.b854f2f7441a09551d6ef8eafcfe7ccc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f984406e40a05eafe54db1fb280c59d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d795c7b1fe2e56478dac",
+    "url": "/static/css/main.3a942757.chunk.css"
+  },
+  {
+    "revision": "98fd30b0d3d6b8ae2e98",
+    "url": "/static/js/2.879f4abd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.879f4abd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d795c7b1fe2e56478dac",
+    "url": "/static/js/main.5dc44c26.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "09bbca81ba3924f1de399f7d88c54df6",
+    "url": "/static/media/new.09bbca81.jpg"
+  }
+]);
